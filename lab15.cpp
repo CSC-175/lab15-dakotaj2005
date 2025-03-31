f53232f1905b3d5d87764c8e755ca880f0f3d0c7
Dakota Back <dakotaj@umich.edu>
2025-03-31 12:56:28 -0400

    1st Attempt

--- lab15.cpp
+++ lab15.cpp
@@ -6,12 +6,26 @@ void getInfo(int&, int&);
 double computeWays(int, int);
 double factorial(int);
 
-/*******************************************************************
-* getLotteryInfo                                                   *
-* Gets and validates lottery info from the user and places it in   *
-* reference parameters referencing variables in the main function. *
-********************************************************************/
+void getInfo(int &n, int &k) {
+while (n < 1 || n > 12) {
+    cout << "How many balls (1-12) are in the pool to pick from? ";
+    cin >> n;
 
+    if (n < 1 || n > 12) {
+        cout << "Invalid input! Please enter a number between 1 and 12!\n";
+        continue;
+    }
+while (k < 1 || k > 7) {
+    cout << "How many balls (1-7) will be drawn? ";
+    cin >> k;
+
+    if (k < 1 || k > 7) {
+        cout << "Invalid input!\n";
+    }
+
+}
+}
+}
 
 
 /*******************************************************************
@@ -20,11 +34,35 @@ double factorial(int);
 * of k numbers that can be chosen from a set of n numbers.         *
 * The formula for this is     k!(n- k)!                            *
 *                             --------                             *
-*                                 n!                               *
-* Note that the computation is done in a way that does not require *
-* multiplying two factorials together. This is done to prevent any *
-* intermediate result becoming so large that it causes overflow.   *
+*                                 n!                               */
+
+
+double computeWays(int n, int k) {
+       return ((factorial(k) * factorial(n - k))/factorial(n));
+}
+
+double factorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else if (n == 1) {
+        return 1;
+    }else {
+        return n * factorial(n - 1);
+    }
+}
+
+
+
+/*******************************************************************
+* getLotteryInfo                                                   *
+* Gets and validates lottery info from the user and places it in   *
+* reference parameters referencing variables in the main function. *
 ********************************************************************/
+
+
+
+
+
 
 
 /*******************************************************************
